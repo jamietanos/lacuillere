65ba4f346beb1d146e5414ab1b05bfb827ad7ce5
Peter Richards <peter.jrichards@yahoo.com>
2017-08-09 09:14:53 +0800

    removed restuarants

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,2 @@
 Rails.application.routes.draw do
-  # get "restaurants", to: "restaurants#index"
-  # get "restaurants/new", to: "restaurants#new"
-  # get "restaurants/:id", to: "restaurants#show"
-  # post "restaurants", to: "restaurants#create"
-  # get "restaurants/:id/edit", to: "restaurants#edit"
-  # patch "restaurants/:id", to: "restaurants#update"
-  # delete "restaurants/:id", to: "restaurants#destroy"
-  resources :restaurants
 end
--- db/migrate/20170808013331_add_address_to_restaurants.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressToRestaurants < ActiveRecord::Migration[5.0]
-  def change
-    add_column :restaurants, :address, :text
-  end
-end
